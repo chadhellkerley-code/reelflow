1e820940bf4fe82045d3f6f9accffff7af66f9a1
chadhellkerley-code <chadhellkerley-code@users.noreply.github.com>
2026-06-01 01:43:45 -0300

    Use TikTok sandbox client key

--- src/assets/app.js
+++ src/assets/app.js
@@ -21,7 +21,7 @@ const state = {
 const PUBLIC_ORIGIN = 'https://reelflow-topaz.vercel.app';
 const INSTAGRAM_APP_ID = '1428803625601557';
 const INSTAGRAM_SCOPES = 'instagram_business_basic,instagram_business_content_publish';
-const TIKTOK_CLIENT_KEY = 'awcuda46dos3vgsv';
+const TIKTOK_CLIENT_KEY = 'sbaw89mga3yconmz26';
 const TIKTOK_SCOPES = 'user.info.basic';
 
 function getPublicOrigin() {
